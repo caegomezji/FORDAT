6d3dedc732eebf34b690f546e7c36a005ea60bbf
Camilo Gomez <caegomezji@gmail.com>
2021-07-30 10:20:57 -0500

    renaming main for fordat

---
+++ fordat.py
@@ -0,0 +1,41 @@
+
+from flask import Flask
+import config
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+## server configurations
+server = Flask( config.app_name )
+app = dash.Dash( config.app_name, external_stylesheets=external_stylesheets , server=server)
+
+## read dataframe
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+
+if __name__ == "__main__":
+    
+    app.run_server(host=config.app_host , port=config.app_port, debug=config.app_debug)
--- main.py
+++
@@ -1,7 +0,0 @@
-
-from flask import Flask
-
-
-app = Flask(config.app_name )
-
-app.run(host="localhost", port="8080")
